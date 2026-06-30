67712c17b44f6536083d09691337a4dc0336ffa9
kirill1943 <255701959+Kirill1943@users.noreply.github.com>
2026-06-30 18:34:53 +0400

    v0.00-2: метод кушанья у чачки, атрибуты xp, eat и жива ли чачка. и обработка методов при мертвой чачке

--- Game/Chachka.py
+++ Game/Chachka.py
@@ -3,11 +3,26 @@
 class Chachka:
     def __init__(self, age):
         self.age = age
+        self.xp = 100
+        self.eat = 100
+        self.alive = True
         self.__size = [6, 6, 6]
     def Viy(self, scream: int = 2):
-        scream = min(5, max(1, scream))
-        print(f'Чачка викает: В{"И" * scream}')
+        if self.alive:
+            scream = min(5, max(1, scream))
+            print(f'Чачка викает: В{'И' * scream}')
+        else:
+            print('Чачка мертва... но кажется она хотела что-то викнуть')
     def Scream(self, scream: int = 10):
-        scream = min(15, max(8, scream))
-        print(f"Чачка орет: В{"И" * scream}")
-
+        if self.alive:
+            scream = min(15, max(8, scream))
+            print(f'Чачка орет: В{'И' * scream}')
+        else:
+            print('чачка мертва.. но она хотела что-то крикнуть')
+    def eating(self):
+        if self.alive:
+            print('Чачка ест')
+            self.eat += 3
+            self.eat = max(0, min(self.eat, 100))
+        else:
+            print('чачка мертва.. но она хотела есть')
