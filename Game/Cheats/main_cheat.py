363d0283aa20a098ab183a3d92f28c474725ef73
kirill1943 <255701959+Kirill1943@users.noreply.github.com>
2026-08-05 20:23:50 +0300

    [v0.01.6] [TUI] добавлен некоторый TUI в главном выборе команд и выборе читов, добавлена зависимость pick

--- Game/Cheats/main_cheat.py
+++ Game/Cheats/main_cheat.py
@@ -1,6 +1,9 @@
 import os
+import subprocess
 import sys
 from typing import Any
+
+from pick import pick
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
@@ -12,18 +15,25 @@ from Game.Map.maps import Map
 
 
 def run(Chack: Chachka, Map: Map, logging_file_path: str):
-    print("=== ЧИТЫ ===")
     while True:
         try:
-            print("[Изменить HP      ] set_hp, hp_set")
-            print("[Изменить сытость ] set_eat, eat_set")
-            print("[Изменить стамину ] set_stamina, stamina_set")
-            print("[Пере-реген. карты] regen_map, map_regen")
-            print("[ Нажмите Ctrl + C или введите exit для выхода из меню читов]")
-            cmd = input().strip().lower()
+            subprocess.run(['cls'] if os.name == 'nt' else ['clear'], shell=False)
+            menu_title = "=== ЧИТЫ ==="
+            options = [
+                "[Изменить HP      ] set_hp, hp_set",
+                "[Изменить сытость ] set_eat, eat_set",
+                "[Изменить стамину ] set_stamina, stamina_set",
+                "[Пере-реген. карты] regen_map, map_regen",
+                "Выйти из меню читов (exit)"
+            ]
+            cmd_keys = ["set_hp", "set_eat", "set_stamina", "regen_map", "exit"]
+            
+            option, index = pick(options, menu_title, indicator='=>')
+            cmd = cmd_keys[index]
+            
             if cmd == "exit": 
                 break
-            if cmd in ["set_hp", "hp_set"]:
+            if cmd == "set_hp":
                 Log.info(f'пользователь выбрал чит: {cmd} (изменение HP)', logging_file_path)
                 hp: Any = input("Введите количество hp (от 0 до 100)")
                 try:
@@ -32,7 +42,8 @@ def run(Chack: Chachka, Map: Map, logging_file_path: str):
                     print('Некорректное количество HP, HP чачки остается прежним')
                 else:
                     cheat_set.set_chachka_hp(hp, chachka=Chack)
-            elif cmd in ["set_eat", "eat_set"]:
+                input("\nНажмите Enter для продолжения...")
+            elif cmd == "set_eat":
                 Log.info(f'пользователь выбрал чит: {cmd} (изменение сытости)', logging_file_path)
                 eat: Any = input("Введите уровень сытости (от 0 до 100)")
                 try:
@@ -41,7 +52,8 @@ def run(Chack: Chachka, Map: Map, logging_file_path: str):
                     print('Некорректное количество сытости, сытость чачки остается прежним')
                 else:
                     cheat_set.set_chachka_eat(eat, chachka=Chack)
-            elif cmd in ["set_stamina", "stamina_set"]:
+                input("\nНажмите Enter для продолжения...")
+            elif cmd == "set_stamina":
                 Log.info(f'пользователь выбрал чит: {cmd} (изменение стамины)', logging_file_path)
                 stamina: Any = input("Введите уровень стамины (от 0 до 100)")
                 try:
@@ -50,9 +62,11 @@ def run(Chack: Chachka, Map: Map, logging_file_path: str):
                     print('Некорректное количество стамины, стамина чачки остается прежним')
                 else:
                     cheat_set.set_chachka_stamina(stamina, chachka=Chack)
-            elif cmd in ["regen_map", "map_regen"]:
+                input("\nНажмите Enter для продолжения...")
+            elif cmd == "regen_map":
                 Log.info(f'пользователь выбрал чит: {cmd} (перерегенерация карты)', logging_file_path)
                 cheat_map.regeneration(Map, mode=str(Map.gen_type))
+                input("\nНажмите Enter для продолжения...")
 
         except KeyboardInterrupt:
             print('Выход...')
